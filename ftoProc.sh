bd7b103b4af362fa530a86a822f2ba24412a9e0e
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-02-27 14:41:06 -0800

    bx-gitRepos-auto

--- ftoProc.sh
+++ ftoProc.sh
@@ -13,13 +13,13 @@ __copying__="
 
 ####+BEGIN: bx:bsip:bash:seed-spec :types "seedFtoCommon.sh"
 SEED="
-*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] |
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/blee/persian-input-method/ftoProc.sh 
+*  /This File/ :: /bisos/git/auth/bxRepos/blee/persian-input-method/ftoProc.sh
 "
 if [ "${loadFiles}" == "" ] ; then
-    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
+    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@"
     exit $?
 fi
 ####+END:
